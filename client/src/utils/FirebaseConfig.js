72d3220417db876e5d8e7dfd951feed7f402bb65
ASHUTOSH-309 <125751575+ASHUTOSH-309@users.noreply.github.com>
2024-06-30 11:18:19 +0530

    Update FirebaseConfig.js

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -5,13 +5,7 @@ import {getAuth} from "firebase/auth"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUBQ3DJgFiajagkoVp4YrYVWlSYCdC2GQ",
-  authDomain: "whatsapp-clone-19d6b.firebaseapp.com",
-  projectId: "whatsapp-clone-19d6b",
-  storageBucket: "whatsapp-clone-19d6b.appspot.com",
-  messagingSenderId: "809456045690",
-  appId: "1:809456045690:web:9077493a6935c2591b75c3",
-  measurementId: "G-H70GVVJYN3"
+
 };
 
 const app = initializeApp(firebaseConfig);
