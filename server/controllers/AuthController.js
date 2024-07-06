7a7fb0a90b23f88f89d4fdf917cf8e60c36346b2
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-07 02:19:51 +0530

    can't send header problem solved

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,4 +1,5 @@
 import getPrismaInstance from "../utils/PrismaClient.js";
+import { generateToken04 } from "../utils/TokenGenerator.js";
 
 export const checkUser = async (req, res, next) => {
   try {
@@ -83,6 +84,33 @@ export const getAllUsers=async (req,res,next)=>{
             
       }
 
+}
+
+export const generateToken=(req,res,next)=>{
+
+      try {
+            console.log("here below")
+            console.log(process.env.ZEGO_APP_ID)
+            const appId=parseInt(process.env.ZEGO_APP_ID);
+            const serverSecret=process.env.ZEGO_APP_SECRET;
+            const userId=req.params.userId;
+            const effectiveTime=3600;
+            const payload=""
+
+            if(appId && serverSecret && userId){
+
+
+                        const token=generateToken04(appId,userId,serverSecret,effectiveTime,payload)
+                     return res.status(200).json({token})
+
+
+            }
+
+                  return res.status(400).send("User id, app id and server secret is required.")
+
+      } catch (error) {
+            next(error)
+      }
 
 
 
